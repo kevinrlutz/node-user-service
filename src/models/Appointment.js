b0d56edd202d1d962a5b73babc989abd7c2621fa
Kevin Lutz <kevinlutz429@gmail.com>
2022-10-17 15:15:08 -0400

    fixed appointment module

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -1,47 +1,47 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const apptSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	type: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	description: {
-		type: String,
-		required: false,
-		trim: true,
-	},
-	startTime: {
-		type: String,
-		required: false,
-		default: new Date().toISOString(),
-		trim: true,
-	},
-	endTime: {
-		type: String,
-		required: false,
-		default: new Date().toISOString(),
-		trim: true,
-	},
-	metadata: {
-		type: String,
-		required: false,
-		trim: true,
-		default: '',
-	},
-	userId: {
-		type: mongoose.Schema.Types.ObjectId,
-		required: true,
-		ref: 'User',
-	},
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  startTime: {
+    type: String,
+    required: false,
+    default: new Date().toISOString(),
+    trim: true,
+  },
+  endTime: {
+    type: String,
+    required: false,
+    default: new Date().toISOString(),
+    trim: true,
+  },
+  metadata: {
+    type: String,
+    required: false,
+    trim: true,
+    default: "",
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
 
-const Appointment = mongoose.model('Appointment', apptSchema)
+const appointment = mongoose.model("appointment", apptSchema);
 
-module.exports = Appointment
+module.exports = appointment;
